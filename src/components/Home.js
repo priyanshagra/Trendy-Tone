a4dbaa6c8bc95a02c3e164816ad46d737e4e8b9f
Rajat <rajatmantri.01@gmail.com>
2023-03-17 23:50:32 +0530

    Changed contact file

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
           <div className="card-body">
             <h5 className="card-title">MENS</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
+              Some quick examples text to build on the card title and make up the
               bulk of the card's content.
             </p>
             <Link to="/men" className="btn btn-primary">
